1a78993e8b2fbf1f768b0ecd4922c004fe0d91c2
Chris Ryu <rjhchris@hotmail.com>
2017-09-18 12:46:49 +0900

    Add files via upload

---
+++ HOL3-2/IoTDeviceRegistration/IoTDeviceRegistration/Models/Command/Command.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models.Commands
+{
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Command
+    {
+        public string Name { get; set; }
+        public List<Parameter> Parameters { get; set; }
+    }
+}
